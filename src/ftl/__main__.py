85f6235e9eb5308844051b25db1282042108de33
Dan Bradham <danielbradham@gmail.com>
2026-02-10 19:03:27 -0500

    Add TaskProgressDialog.

--- src/ftl/__main__.py
+++ src/ftl/__main__.py
@@ -91,11 +91,18 @@ def editor():
 def encode(folder: Path = Path("."), recursive: bool = False, max_depth: int = 2):
     """Encode a folder of sequences."""
 
+    from ftl import ui
+
     results = []
     if not recursive:
         task = tasks.EncodeFolder(folder, get_settings())
+
+        # Show progress dialog...
+        ui.TaskProgressDialog.from_tasks([task])
+
         task()
         results.extend(task.result)
+
     else:
         folders = set()
         for root, subdirs, _ in os.walk(folder, topdown=True):
@@ -103,10 +110,16 @@ def encode(folder: Path = Path("."), recursive: bool = False, max_depth: int = 2
                 subdirs[:] = []
             folders |= set([seq.path.parent for seq in tasks.get_sequences(Path(root))])
 
-        results = []
+        task_group = []
         for i, folder in enumerate(folders):
             print(f"Encode Folder {i + 1} of {len(folders)}")
             task = tasks.EncodeFolder(folder, get_settings())
+            task_group.append(task)
+
+        # Show progress dialog...
+        ui.TaskProgressDialog.from_tasks(task_group)
+
+        for task in task_group:
             task()
             results.extend(task.result)
 
--- src/ftl/const.py
+++ src/ftl/const.py
@@ -2,6 +2,7 @@ from ftl import resources
 
 FONT_FILE = resources.get("CommitMono.ttf").as_posix()
 ICON_FILE = resources.get("ftl.ico").as_posix()
+ICON_ENCODE_FILE = resources.get("encode.ico").as_posix()
 SIZE_ITEMS = [-1, 4096, 3840, 2048, 1920, 1280, 1024, 768, 512]
 FPS_ITEMS = [12, 15, 24, 30, 60]
 GIF_SIZE_ITEMS = [-1, 4096, 3840, 2048, 1920, 1280, 1024, 768, 512]
